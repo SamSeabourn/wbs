2b9865937d5ac55ee75bfd15f49e9fda829f7837
Sam Seabourn <sam.seabourn@gmail.com>
2025-12-19 22:56:16 +1100

    minify file sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
+import Compress from '@playform/compress';
+
 export default defineConfig({
 	output: 'static',
 	site: 'https://samseabourn.github.io',
 	base: '/wbs/',
-
-	// @ts-expect-error No type here yet, upstream PR maybe?
-	integrations: [tailwindcss()],
-
-	build: {
-		inlineStylesheets: 'always',
-	},
-
+	integrations: [
+		// @ts-expect-error No types here yet, upstream PR maybe?
+		tailwindcss(),
+		Compress({
+			CSS: true,
+			HTML: true,
+			JavaScript: true,
+			SVG: true,
+		}),
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
